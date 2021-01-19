e7e70c33639f413d025bf4ca2e5b6f92f5fb1400
Mustafa <startmk2020@gmail.com>
2021-01-19 16:21:16 +0100

    fin exo 9

--- src/component/Navigation.js
+++ src/component/Navigation.js
@@ -1,13 +1,27 @@
 import './Navigation.css';
 
 function Navigation() {
+    const accueil = ()=>{
+
+      console.log('Accueil');
+
+    }
+
+    const Galerie =()=>{
+      console.log(`Galerie`);
+    }
+
+    const Contact =()=>{
+      console.log(`Contact`);
+    } 
+
     return (
       <div className="Navigation">
 
           <ul>
-              <li> Link 1 </li>
-              <li> Link 3</li>
-              <li> Link 2</li>
+              <li onClick={accueil}> Link 1 </li>
+              <li onMouseOver={Galerie}> Link 3</li>
+              <li onDoubleClick={Contact}> Link 2</li>
           </ul>
        
       </div>
